5dc5b2e9849c923423cad6244ac823c6c3f7d538
kevin <33508488+kjun9@users.noreply.github.com>
2018-09-16 23:17:09 +1000

    Experiments with files and sha

--- src/main/scala/org/theresnobox/nobox/App.scala
+++ src/main/scala/org/theresnobox/nobox/App.scala
@@ -1,9 +1,53 @@
 package org.theresnobox.nobox
 
+import cats.data.EitherT
+import cats.effect.IO
+import java.io.File
+import java.security.MessageDigest
+import scala.io.Source
+
 object App {
 
-  def main(args : Array[String]) : Unit = {
+  def main(args: Array[String]): Unit = {
     println("Hello world!")
+  }
+
+  def ls(f: File): EitherT[IO, Throwable, Seq[File]] = 
+    EitherT(IO {
+      f.listFiles.toSeq
+    }.attempt)
+
+  def empty: EitherT[IO, Throwable, Map[String, String]] =
+    EitherT.rightT[IO, Throwable](Map())
+
+  def read(f: File): EitherT[IO, Throwable, Map[String, String]] = 
+    if (f.isFile) EitherT(
+      IO {
+        Map(f.getName -> Source.fromFile(f).mkString)
+      }.attempt)
+    else if (f.isDirectory) for {
+        contents <- ls(f)
+        strContents <- contents.foldLeft(empty) {
+          (acc, con) => for {
+            a <- acc
+            c <- read(con)
+          } yield a ++ c
+        }
+      } yield strContents
+    else empty
+
+  def getSha(f: File): EitherT[IO, Throwable, Seq[(String, String)]] = for {
+    shas <- read(f)
+  } yield shas.toSeq map {
+    case (name, sha) =>
+      (
+        name,
+        MessageDigest
+          .getInstance("SHA-256")
+          .digest(sha.getBytes("UTF-8"))
+          .map("%02x".format(_))
+          .mkString
+      )
   }
 
 }
